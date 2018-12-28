1ccf2da1340d28d7471e5bb349f2899e4836d811
osdnk <micosa97@gmail.com>
2018-12-28 23:41:03 +0100

    Sth moar

--- example/App.js
+++ example/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import { Platform, StyleSheet, Text, View, Animated, findNodeHandle, TextInput } from 'react-native';
 import { connect } from 'react-native-animated-props'
+/*
 setInterval(() => {
   let iters = 1e8, sum = 0;
   while (iters-- > 0) sum += iters;
 }, 100);
+*/
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -13,13 +16,31 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+
+
+//  LOGICAL SET
+
+
+//const mulitply = (...args) => args.reduce((acc, v) => Animated.multiply(acc, v), 1);
+
+//const add = (...args) => args.reduce((acc, v) => Animated.add(acc, v), 0);
+
+const cond = (_if, _then, _else = 0) => Animated.add(Animated.multiply(_if, _then), Animated.multiply(Animated.subtract(1, _if), _else));
+
+
+const greaterThan = (val, from) => val.interpolate({ inputRange: [from - 0.0001, from, from, from + 0.0001], outputRange: [0, 0, 1, 1] });
+const greaterOrEq = (val, from) => val.interpolate({ inputRange: [from - 0.0001, from - 0.0001, from, from + 0.0001], outputRange: [0, 0, 1, 1] });
+const lesserOrEq = (val, from) => val.interpolate({ inputRange: [from - 0.0001, from, from, from + 0.0001], outputRange: [1, 1, 0, 0] });
+const lesserThan = (val, from) => val.interpolate({ inputRange: [from - 0.0001, from - 0.0001, from, from + 0.0001], outputRange: [1, 1, 0, 0] });
+
 export default class App extends Component {
   ref = React.createRef()
   ref2 = React.createRef()
   ref3 = React.createRef()
-  val = new Animated.Value(10)
+  val = new Animated.Value(149)
   val2 = new Animated.Value(10)
-  componentDidMount() {
+  constructor(props) {
+    super(props)
     Animated.timing(
       // Animate value over time
       this.val, // The value to drive
@@ -38,7 +59,7 @@ export default class App extends Component {
             useNativeDriver: true
           }).start()
       }
-    ); // Start the animation
+    ); // Start the animation*/
 
     Animated.timing(
       // Animate value over time
@@ -59,31 +80,33 @@ export default class App extends Component {
           }).start()
       }
     ); // Start the animation
+    this.coni = cond(lesserThan(this.val, 150), this.val, this.val);
+    console.log(this.coni)
 
 
-    this.val.addListener(({ value }) => {
-      this.ref.current.setNativeProps({
-        style: {
-          width: value
-        }
-      })
-    })
+    //this.val.addListener((e) => console.log(e))
 
-/*    this.val.addListener(({ value }) => {
-      this.ref3.current.setNativeProps({
-        text: value.toString(),
-        value: value,
-        style: {
-          width: value
-        }
-      })
-    })*/
+
+
+    /*    this.val.addListener(({ value }) => {
+          this.ref3.current.setNativeProps({
+            text: value.toString(),
+            value: value,
+            style: {
+              width: value
+            }
+          })
+        })*/
     //console.warn(this.val.__getNativeTag())
     //setTimeout(()=>connect(this.ref2.current, this.val2, "width"), 200);
-    connect(this.ref2.current, this.val2, "width")
-    connect(this.ref3.current, this.val2, "text", "string")
+
+    //connect(this.ref.current, coindVal, "width")
+    //connect(this.ref3.current, this.val2, "text", "string")
     //connect(this.ref2.current, this.val2, "height")
     //RNAnimatedProps.connect(findNodeHandle(this.ref.current), this.val.__getNativeTag())
+  }
+  componentDidMount() {
+    connect(this.ref2.current, this.coni, "width")
   }
   render() {
     return (
@@ -102,7 +125,7 @@ export default class App extends Component {
             backgroundColor: 'red'
           }}
         />
-       <View
+       <Animated.View
           ref={this.ref2}
           style={{
             width: 100,
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { RNAnimatedProps } = NativeModules;
 
 export function connect(view, node, on, cast = "number") {
   const vieTag = findNodeHandle(view);
+  node.__makeNative()
   const nativeNodeTag = node.__getNativeTag()
   RNAnimatedProps.connect(vieTag, nativeNodeTag, on, cast)
 
